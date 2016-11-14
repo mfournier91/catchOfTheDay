ca1db7793fda355d243d0b76ba36e0ed2a4eb5e9
Marc Fournier <marc.fournier91@gmail.com>
2016-11-14 15:15:29 -0500

    Jello World

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom'; //get the method called render in react-dom and assign it to render
+
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker/>, document.getElementById('main'));
