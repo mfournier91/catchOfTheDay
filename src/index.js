0e16f48dcc448a00792b3a03d4023e54952abacf
Marc Fournier <marc.fournier91@gmail.com>
2016-11-14 15:56:17 -0500

    Rendering app component on start which has 3 child components Header Order and Inventory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom'; //get the method called render in react-dom and assign it to render
-
+import './css/style.css';
 import StorePicker from './components/StorePicker';
+import App from './components/app'
 
-
-render(<StorePicker/>, document.getElementById('main'));
+render(<App/>, document.getElementById('main'));
