ed77ec666f013437d2fa43533f56deccf0b5963f
Marc Fournier <marc.fournier91@gmail.com>
2016-11-14 18:31:11 -0500

    Input grabs a value from a function getFunName. On form submission passes a storeInput attr to the component which is equal to the input html element.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component {
+  constructor(){
+    super();
+    this.goToStore = this.goToStore.bind(this); //Makes "this" inside of goToStore refer to StorePicker
+  }
+
+  goToStore(e) {
+    e.preventDefault();
+    console.log(this.storeInput.value);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         {/* Ugh jsx comments are ugly */}
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>  //random name and on submission gives a property storeInput to StorePicker whose value is this input html element
         <button type="submit">Visit store</button>
       </form>
     )
