2a78b2d7e82f435281e3ae3eff1dcfdd191fee95
Marc Fournier <marc.fournier91@gmail.com>
2016-11-14 17:04:10 -0500

    React router: Match and Miss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom'; //get the method called render in react-dom and assign it to render
 import './css/style.css';
-import StorePicker from './components/StorePicker';
-import App from './components/app'
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-render(<App/>, document.getElementById('main'));
+import StorePicker from './components/StorePicker';
+import App from './components/app';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.getElementById('main'));
