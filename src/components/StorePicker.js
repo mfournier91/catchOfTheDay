ea2e295202da95384bdaf0ca8b891e03b653ab76
Marc Fournier <marc.fournier91@gmail.com>
2016-11-14 15:29:28 -0500

    StorePicker component returns a static form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Jello World</p>
+    return (
+      <form className="store-selector">
+        {/* Ugh jsx comments are ugly */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit store</button>
+      </form>
+    )
   }
 }
 
