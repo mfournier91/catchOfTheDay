c969936dc856941c4eddd89ae030c49aa8c86541
Marc Fournier <marc.fournier91@gmail.com>
2016-11-18 14:21:19 -0500

    Sync app with firebase.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDw0J1j89eZ23aAvBnyC8-rYwBJwEGv5Ms",
+  authDomain: "catchofthedaymarc.firebaseapp.com",
+  databaseURL: "https://catchofthedaymarc.firebaseio.com",
+});
+
+export default base;
