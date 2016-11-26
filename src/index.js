86a2e42be4e16e7fdb4f5e02c91087029a15bb76
Marc Fournier <marc.fournier91@gmail.com>
2016-11-25 21:42:22 -0500

    Setting things up for github pages hosting.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import NotFound from './components/NotFound';
 
 const Root = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/catchOfTheDay/">
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match pattern="/store/:storeId" component={App} />
